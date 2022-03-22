13c26c924a9092d9cd83703fbf49b9c9aa8c5f4a
CollinClifford <collinclifford@ymail.com>
2022-03-22 10:13:02 -0700

    fixed

--- src/server.js
+++ src/server.js
@@ -1,19 +1,5 @@
 const { PORT = 5000 } = process.env;
-
 const app = require("./app");
 const knex = require("./db/connection");
-
-knex.migrate
-  .latest()
-  .then((migrations) => {
-    console.log("migrations", migrations);
-    app.listen(PORT || 3000, listener);
-  })
-  .catch((error) => {
-    console.error(error);
-    knex.destroy();
-  });
-
-function listener() {
-  console.log(`Listening on Port ${PORT}!`);
-}
+const listener = () => console.log(`Listening on Port ${PORT}!`);
+app.listen(PORT, listener);
