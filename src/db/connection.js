42fb5b43c284066870c69563224bfcb264813c0a
CollinClifford <collinclifford@ymail.com>
2022-03-22 10:19:25 -0700

    ts

--- src/db/connection.js
+++ src/db/connection.js
@@ -1,6 +1,5 @@
 const env = process.env.NODE_ENV || "development";
 const config = require("../../knexfile")[env];
-console.log(config)
 const knex = require("knex")(config);
 
 module.exports = knex;
