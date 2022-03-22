18dff5f04225325d6d9604cbfef3a8f0fff7a3e9
CollinClifford <collinclifford@ymail.com>
2022-03-22 11:22:23 -0700

    hei

--- knexfile.js
+++ knexfile.js
@@ -1,7 +1,7 @@
 const path = require("path");
 require("dotenv").config();
 const { DATABASE_URL } = process.env;
-
+console.log(DATABASE_URL);
 module.exports = {
   development: {
     client: "postgresql",
@@ -13,7 +13,8 @@ module.exports = {
       directory: path.join(__dirname, "src", "db", "seeds"),
     },
   },
-  production: {
+  client: {
+    client: "postgresql",
     connection: DATABASE_URL,
     migrations: {
       directory: path.join(__dirname, "src", "db", "migrations"),
